d50dc8a7e83021e7c1e7ff1cc278c3bd070d1fcd
sungmin kim <ignocide@gmail.com>
2017-09-09 22:38:17 +0900

    * facebook 인증

--- routes/auth.js
+++ routes/auth.js
@@ -5,43 +5,59 @@ const router = express.Router()
 
 const passport = require('passport')
 const serviceToken = require('../services/token')
+const modelUser = require('../models/user')
 
+const co = require('co')
+const FB = require('fb')
 /* GET posts list */
-router.get('/facebook',
-  passport.authenticate('facebook'))
+router.post('/facebook',
+  function (req, res, next) {
+    co(function * () {
+      var uid = req.body.id
 
-/* POST posts create */
-router.get('/facebook/callback',
-  passport.authenticate('facebook', { failureRedirect: '/facebook/fail' }),
+      var profile = yield getProfile.bind(null, req.body.accessToken)
+      var user = yield modelUser.read.bind(null, uid)
+      if (user.length) {
+        res.locals.user = user
+        return next()
+      }
+      var date = getTime()
+      user = {
+        uid: uid,
+        name: profile.displayName,
+        ctime: date,
+        utime: date
+      }
+      yield modelUser.create.bind(null, user)
+      res.locals.user = user
+      return next()
+    }).catch(function (err) {
+      res.status(500).json({
+        success: false,
+        error: err
+      })
+    })
+  },
   function (req, res) {
-    // Successful authentication, redirect home.
-    var token = serviceToken.generate(req.user)
+    var token = serviceToken.generate(res.locals.user)
     res.json({
       success: true,
-      result: {
-        token: token
+      token: token
+    })
+  }
+)
+
+var getProfile = function (accessToken, callback) {
+  FB.options({accessToken: accessToken})
+
+  FB.api('/me', function (res) {
+    if (res && res.error) {
+      if (res.error) {
+        return callback(res.error)
       }
-    })
+    } else {
+      return callback(null, res)
+    }
   })
-
-/* POST posts create */
-router.get('/facebook/failure',
-  function (req, res) {
-    // Successful authentication, redirect home.
-    res.json({
-      success: false
-    })
-  })
-
-// // dumy
-// router.get('/test', function (req, res) {
-//   var user = { uid: '1385393824907130',
-//     name: 'SungMin  Kim',
-//     ctime: 1504944344121,
-//     utime: 1504944344121 }
-//   repoUser.create(user, function () {
-//     console.log(arguments)
-//   })
-// })
-
+}
 module.exports = router
